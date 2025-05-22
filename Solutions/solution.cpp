7fb6090fa78e0b33746c97051fe8d0bf77536918
Tejaswini Vaddemgunta <tejaswini.v317@gmail.com>
2025-05-23 00:23:24 +0530

    Update solution.cpp

--- Solutions/solution.cpp
+++ Solutions/solution.cpp
@@ -1,14 +1,19 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 int leastInterval(vector<char>& tasks, int n) {
     vector<int> freq(26, 0);
     for (char t : tasks) freq[t - 'A']++;
     sort(freq.begin(), freq.end());
-    int max = freq[25] - 1;
-    int idle = max * n;
-    for (int i = 24; i >= 0 && freq[i] > 0; i--) {
-        idle -= min(freq[i], max);
+    
+    int maxFreq = freq[25];
+    int idleSlots = (maxFreq - 1) * n;
+
+    for (int i = 24; i >= 0 && idleSlots > 0; i--) {
+        idleSlots -= min(freq[i], maxFreq - 1);
     }
-    return idle > 0 ? tasks.size() + idle : tasks.size();
+    idleSlots = max(0, idleSlots);
+
+    return tasks.size() + idleSlots;
 }
